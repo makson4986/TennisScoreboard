abf5c972b6150d01e969ad04616a1f1124ed2fbe
makson <maksbatkaev06@gmail.com>
2025-03-31 17:22:31 +0300

    create: unit tests

---
+++ src/test/java/org/makson/tennisscoreboard/service/MatchScoreCalculationServiceTest.java
@@ -0,0 +1,95 @@
+package org.makson.tennisscoreboard.service;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.makson.tennisscoreboard.dto.Match;
+import org.makson.tennisscoreboard.models.Player;
+import org.makson.tennisscoreboard.services.MatchScoreCalculationService;
+
+class MatchScoreCalculationServiceTest {
+    private MatchScoreCalculationService matchScoreCalculationService;
+    private Match match;
+    private final String PLAYER1 = "playerOne";
+    private final String PLAYER2 = "playerTwo";
+
+    @BeforeEach
+    void initScore() {
+        match = new Match(
+                Player.builder().name("test1").build(),
+                Player.builder().name("test2").build()
+        );
+
+
+        matchScoreCalculationService = MatchScoreCalculationService.getInstance();
+    }
+
+    @Test
+    @DisplayName("If player 1 wins a point at 40-40, the game does not end")
+    void gameNotEndIfPlayerOneWinPointInDeuce() {
+        match.getScorePlayerOne().setPoints(40);
+        match.getScorePlayerTwo().setPoints(40);
+        match.setDeuce(true);
+
+        matchScoreCalculationService.calculateMatchScore(match, PLAYER1);
+
+        Assertions.assertEquals(0, match.getScorePlayerOne().getGames());
+    }
+
+    @Test
+    @DisplayName("If player 1 wins a point when the score is 40-0, then he also wins the game")
+    void winGameIfPlayerOneWinPointInScore40To0() {
+        match.getScorePlayerOne().setPoints(40);
+        match.getScorePlayerTwo().setPoints(0);
+
+        matchScoreCalculationService.calculateMatchScore(match, PLAYER1);
+
+        Assertions.assertEquals(1, match.getScorePlayerOne().getGames());
+    }
+
+    @Test
+    @DisplayName("If player 1 wins the game with the score 6-5 then a tie-break begins")
+    void beginsTieBreakIfPlayerOneWinGameInScore6To5() {
+        match.getScorePlayerOne().setPoints(40);
+        match.getScorePlayerTwo().setPoints(0);
+
+        match.getScorePlayerOne().addGames(5);
+        match.getScorePlayerTwo().addGames(6);
+
+        matchScoreCalculationService.calculateMatchScore(match, PLAYER1);
+
+        Assertions.assertTrue(match.isTieBreak());
+    }
+
+    @Test
+    @DisplayName("If player 1 does not win the tie-break at 9-8")
+    void playerOneNotWinInTieBreakInScore9To8() {
+        match.getScorePlayerOne().setPoints(8);
+        match.getScorePlayerTwo().setPoints(8);
+
+        match.getScorePlayerOne().addGames(6);
+        match.getScorePlayerTwo().addGames(6);
+        match.setTieBreak(true);
+
+        matchScoreCalculationService.calculateMatchScore(match, PLAYER1);
+
+        Assertions.assertTrue(match.isTieBreak());
+    }
+    @Test
+    @DisplayName("Player 1 wins a match if he wins a set with the score 1-1 in the set")
+    void playerOneWinMatchIfWinSetInScore1To1() {
+        match.getScorePlayerOne().setPoints(40);
+        match.getScorePlayerTwo().setPoints(4);
+
+        match.getScorePlayerOne().addGames(6);
+        match.getScorePlayerTwo().addGames(2);
+
+        match.getScorePlayerOne().addSets(1);
+        match.getScorePlayerTwo().addSets(1);
+
+        matchScoreCalculationService.calculateMatchScore(match, PLAYER1);
+
+        Assertions.assertTrue(match.isFinished());
+    }
+}
